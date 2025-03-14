a91060b44b2d7cf9b347a8a3caca6f9ba150b0e5
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 02:19:34 +0000

    Inserindo dados na tabela salario_classe

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,5 @@
--- Insira 3 classes salariais na tabela salario_classe
-
-
--- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+-- SQLite
+INSERT INTO salario_classe (salario, nivel)
+VALUES 
+(4700, 'gerencia'),
+(5300, 'Coordenador');
