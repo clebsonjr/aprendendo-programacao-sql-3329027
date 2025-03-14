62c2039af132ad359883b3ca8ae9a3149cdecbb7
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 14:50:31 +0000

    Criando uma busca por valores únicos com DISTINCT para abreviações dos estados e trocando pelo nome por extenso do mesmos como 'SP' por 'SÃO PAULO'. Usando CASE.

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,12 @@
--- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT
+Country AS pais,
+State AS estado_sigla,
+CASE
+  WHEN State = 'SP' THEN 'SÃO PAULO'
+  WHEN State = 'RJ' THEN 'RIO DE JANEIRO'
+  ELSE 'ESTADO DESCONHECIDO'
+END AS estado
+FROM
+customers
+WHERE
+Country LIKE 'Brazil';
