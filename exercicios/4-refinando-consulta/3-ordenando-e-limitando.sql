0babea2cad8aa98d29e9a5f445e26b68ca139504
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 19:31:15 +0000

    Ordenando e limitando a buscar por dados no banco.

--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,14 @@
--- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
+--ordenando e limitando as consultas
+SELECT
+CustomerId as id,
+FirstName as nome,
+Country as pais
+FROM
+customers
+ORDER BY
+pais ASC,
+nome DESC
+LIMIT 10;
 
-
--- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
-
-
--- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+--asc forma crescente
+--desc forma decrescente
