3bfa45210a99e5181cad95030fc35104a7a4427e
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 19:36:24 +0000

    fazendo buscas de valores únicos e mudando o nome da coluna para uma palavra do meu idioma.

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,7 +1,8 @@
--- Para a tabela "customers" crie uma consulta que retorne apenas os valores únicos da coluna Country
+SELECT DISTINCT
+Country as pais,
+Company as empresa
+FROM
+customers;
 
-
--- Na consulta anterior, adicione a coluna Company para verificar as combinações únicas entre empresas e países
-
-
--- Nesta mesma consulta, atribua um alias com título em português para cada coluna
+--fazendo buscas de valores únicos e mudando o nome
+--da coluna para uma palavra do meu idioma.
