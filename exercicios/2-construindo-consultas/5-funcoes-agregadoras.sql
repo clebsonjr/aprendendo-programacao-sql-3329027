49bf32cc1c12160a90caced8b51248aa36d7dff4
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 15:28:07 +0000

    Aplicando as funções no BD invoices

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,1 +1,16 @@
--- Para a tabela "invoices", crie uma consulta que retorne o país e a soma das compras realizadas nele, bem como o valor da compra mínima e máxima, a quantidade de compras realizadas e o ticket médio de cada compra.
+SELECT
+BillingCountry as pais,
+SUM(Total) as soma_compras,
+COUNT(Total) as qtd_compras_realizadas,
+MIN(Total) as menor_compra,
+MAX(Total) as maior_compra,
+ROUND(AVG(Total), 2) as ticket_medio
+FROM
+invoices
+WHERE
+BillingCountry = 'Austria';
+
+--SUM Soma a coluna de Total
+--Count conta a quantidade de compras,um contator
+--MAX e MIN busca os valores maxímo e minímo
+--ROUN faz o arredondamento da média AVG e comverte para duas casa o resultado
