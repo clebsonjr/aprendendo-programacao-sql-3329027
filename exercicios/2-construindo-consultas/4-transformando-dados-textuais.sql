76d9e06fc74618d7a33bb64bc421f3bd831a7b85
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 15:08:06 +0000

    Aplicando mudanças nos dados de textos.

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,13 @@
--- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
+SELECT DISTINCT
+CustomerId as id,
+LOWER(FirstName) as nome,
+UPPER(LastName) as sobrenome,
+FirstName || '' || LastName as nome_completo,
+REPLACE(Address, 'Av.', 'Avenida') as endereco
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
-
--- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
-
--- Na consulta anterior, converta o nome para letras minúsculas
-
-
--- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
-
-
--- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
-
-
--- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+--LOWER COLOCA UMAPALAVRA TODA COM LETRA MINUSCÚLA
+--UPPER COM AS LETRAS TODAS MAIÚSCULA
+--REPLACE USAMOS PARA TRANSFORMAR ABREVEAÇÕES EM PALAVRA ESCRITA POR EXTENSO
