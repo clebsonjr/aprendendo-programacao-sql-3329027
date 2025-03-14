ac432678cdc7fd95199f6e8cd3a20e0f2ecc5790
ClebsonMelo <31661635+clebsonjr@users.noreply.github.com>
2025-03-14 14:28:27 +0000

    Aplicando operadores no BD Invoice_Items

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,10 @@
--- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
-
--- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
-
--- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+TrackId,
+UnitPrice,
+UnitPrice * 0.5,
+UnitPrice + 2.11,
+(UnitPrice + 2.11) % 2
+FROM
+invoice_items
+WHERE InvoiceId = 3
+AND InvoiceId < 10;
